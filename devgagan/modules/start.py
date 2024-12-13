707f3633575ed8febf82f482a0d753c4a34cfd33
devilnatkhat <telegram7860@gmail.com>
2024-12-13 20:12:28 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -9,8 +9,8 @@ from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardBu
 
 buttons = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton("Join Channel", url="https://t.me/save_restricted_content_bots")],
-        [InlineKeyboardButton("Buy Premium", url="https://t.me/kingofpatal")]
+        [InlineKeyboardButton("Join Channel", url="https://t.me/Tripathi_bots")],
+        [InlineKeyboardButton("Buy Premium", url="https://t.me/Tripathijicoms")]
     ]
 )
 
